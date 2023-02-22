0d9797a61a390b444f5955b9caf19a3752cbaf22
aliifam <aliif.arief.m@gmail.com>
2023-02-22 09:14:57 +0700

    init project

---
+++ app/src/main/java/com/aliif/app1/SimpleFragment.java
@@ -0,0 +1,14 @@
+package com.aliif.app1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SimpleFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.simple_fragment);
+    }
+}
