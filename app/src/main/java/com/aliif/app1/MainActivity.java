74b4f3d086de863102b8ecd944888aab252eb084
aliifam <aliif.arief.m@gmail.com>
2023-02-28 09:17:56 +0700

    finishing tugas fragment statis motor PCX bos senggol dong

--- app/src/main/java/com/aliif/app1/MainActivity.java
+++ app/src/main/java/com/aliif/app1/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            Objects.requireNonNull(this.getSupportActionBar()).hide();
+        }catch (NullPointerException ignored){
+        }
         setContentView(R.layout.activity_main);
     }
 }
