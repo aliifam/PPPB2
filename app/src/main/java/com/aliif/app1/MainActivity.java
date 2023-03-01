94428f81abd3441f0212f82ad54b571d3b36f8db
aliifam <aliif.arief.m@gmail.com>
2023-03-01 08:41:46 +0700

    finishing all

--- app/src/main/java/com/aliif/app1/MainActivity.java
+++ app/src/main/java/com/aliif/app1/MainActivity.java
@@ -1,14 +1,13 @@
 package com.aliif.app1;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Objects;
 
@@ -29,45 +28,37 @@ public class MainActivity extends AppCompatActivity {
 
         button = findViewById(R.id.tombol);
 
-        Fragment fragment = ProductInfo.newInstance();
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.)
-
-//        FragmentManager fragmentManager = getSupportFragmentManager();
-//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//        ProductInfo productInfo = new ProductInfo();
-//        fragmentTransaction.add(R.id.fragment, productInfo).commit();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        ProductInfo productInfo = new ProductInfo();
+        fragmentTransaction.add(R.id.fragment, productInfo).commit();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Fragment fragment
-//                if(!isFirst){
-//                    changeFragment();
-//                }else {
-//                    displayFragment();
-//                }
+                if(isFirst){
+                    changeFragment();
+                }else {
+                    displayFragment();
+                }
             }
         });
     }
 
-//    private void changeFragment(){
-//        FragmentManager fragmentManager = getSupportFragmentManager();
-//        ProductBenefit productBenefit = ProductBenefit.newInstance();
-//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//        fragmentTransaction.replace(R.id.fragment, productBenefit).commit();
-//
-//        isFirst = false;
-//    }
-//
-//    private void displayFragment(){
-//        FragmentManager fragmentManager = getSupportFragmentManager();
-//        ProductBenefit productBenefit = (ProductBenefit) fragmentManager.findFragmentById(R.id.fragment);
-//
-//        if(productBenefit != null){
-//            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//            fragmentTransaction.replace(R.id.fragment, new ProductBenefit()).commit();
-//        }
-//
-//        isFirst = true;
-//    }
+    private void changeFragment(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        ProductBenefit productBenefit = ProductBenefit.newInstance();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment, productBenefit).commit();
+
+        isFirst = false;
+    }
+
+    private void displayFragment(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        ProductInfo productInfo = ProductInfo.newInstance();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment, productInfo).commit();
+
+        isFirst = true;
+    }
 }
