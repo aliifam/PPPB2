e99cab12ba555aab254d85577f847a0c10cb58ce
aliifam <aliif.arief.m@gmail.com>
2023-03-07 09:56:41 +0700

    finisihed week3

--- app/src/main/java/com/aliif/week3/WidgetPertama.java
+++ app/src/main/java/com/aliif/week3/WidgetPertama.java
@@ -1,22 +1,50 @@
 package com.aliif.week3;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.widget.RemoteViews;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class WidgetPertama extends AppWidgetProvider {
 
+    private static final String widgetSharePref =  "com.aliif.week3";
+    private static final String KEY = "count";
+
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
+        SharedPreferences sharedPreferences = context.getSharedPreferences(widgetSharePref, 0);
+        int count = sharedPreferences.getInt(KEY+appWidgetId, 0);
+
+        count++;
+
+        String dateString = DateFormat.getTimeInstance(DateFormat.SHORT).format(new Date());
+
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_pertama);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.appwidget_id, String.valueOf(appWidgetId));
+        views.setTextViewText(R.id.appwidget_update, dateString);
+        views.setTextViewText(R.id.appwidget_count, String.valueOf(count));
+
+        Intent intent = new Intent(context, WidgetPertama.class);
+        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+
+        int[] idArray = new int[] {appWidgetId};
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, idArray);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, appWidgetId, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        views.setOnClickPendingIntent(R.id.button_update, pendingIntent);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(KEY+appWidgetId, count);
+        editor.apply();
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
