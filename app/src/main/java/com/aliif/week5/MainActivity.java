845914f9354e68021cea9818e03517dfa6dbe158
aliifam <aliif.arief.m@gmail.com>
2023-03-15 09:54:20 +0700

    feat: backboning contract method

--- app/src/main/java/com/aliif/week5/MainActivity.java
+++ app/src/main/java/com/aliif/week5/MainActivity.java
@@ -2,13 +2,77 @@ package com.aliif.week5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+
+    private Sensor accelerometer;
+    private Sensor magnetometer;
+
+    private TextView textAzimuth;
+    private TextView textPitch;
+    private TextView textRoll;
+
+    private float[] accelerometerData = new float[3];
+    private  float[] magnetometerData = new float[3];
+
+    private ImageView spotTop;
+    private ImageView spotBottom;
+    private ImageView spotRight;
+    private ImageView spotLeft;
+
+    private static final float VALUE_DRIFT = 0.05f;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        textAzimuth = findViewById(R.id.value_azimuth);
+        textPitch = findViewById(R.id.value_pitch);
+        textRoll = findViewById(R.id.value_roll);
+
+        spotTop = findViewById(R.id.spot_top);
+        spotBottom = findViewById(R.id.spot_bottom);
+        spotRight = findViewById(R.id.spot_right);
+        spotLeft = findViewById(R.id.spot_left);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
